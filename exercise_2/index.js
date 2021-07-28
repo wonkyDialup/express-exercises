fd32211b07e4e6a0d8eac742bcb4f366c71ad4f1
Joseph Frasier <30413292+jwfrasier@users.noreply.github.com>
2021-07-28 15:10:51 -0500

    starting on exercise 2
    starting on exercise 2

---
+++ exercise_2/index.js
@@ -0,0 +1,13 @@
+const PORT = 3001
+const express = require("express")
+const app = express()
+
+// Write your routes between the ***
+
+// ***
+
+// ***
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
---
+++ exercise_2/instructions.md
@@ -0,0 +1,4 @@
+# Instructions
+
+## Where are the routes??
+- You will have to create routes here that will respond back with some wonderful messages!
